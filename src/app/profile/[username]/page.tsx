26ff237d3b7cdaf777c73d95b5e6d4c5b3fcf789
shaheem <shaheemmahmood728@gmail.com>
2025-08-16 17:04:21 +0530

    create user profile page

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,33 @@
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
+
+async function ProfilePageServer({ params }: { params: Promise<{ username: string }> }) {
+  const { username } = await params; // await params
+
+  const user = await getProfileByUsername(username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+
+export default ProfilePageServer;
